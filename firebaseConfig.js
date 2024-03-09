7049dfe4aa96ef48751c4bb78e3859907338b7d7
Shane Shaji Thomas <shaneshaji10@gmail.com>
2024-03-09 19:55:35 +0530

    chore: added firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5960zRGCkdFdYlL62z41u2sH5-98Nzuc",
+  authDomain: "women-techies-24.firebaseapp.com",
+  projectId: "women-techies-24",
+  storageBucket: "women-techies-24.appspot.com",
+  messagingSenderId: "734955422127",
+  appId: "1:734955422127:web:71fbf4e86d57684ecfef5a",
+  measurementId: "G-GPM7LWRNBM",
+};
+
+const app = initializeApp(firebaseConfig);
